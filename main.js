ca8bc63bacbae83812766791e4e2cabd39cd71c4
Josue Caal <josuecaal787@gmail.com>
2024-05-08 16:56:56 -0600

    cargar archivo

---
+++ main.js
@@ -0,0 +1,32 @@
+//color de estrallas
+const COLORS =["#fff2","#fff4","#fff7","#fffc"];
+//generar estrellas
+const generateSpaceLAyer = (size, selector, totalStars, duration) =>{
+    //generar array para cordenadas de estrellas
+    const layer =[];
+
+    for (let i = 0; i < totalStars; i++) {
+        //recorrer array colors
+        const color = COLORS[Math.floor(Math.random()*COLORS.length)];
+        //cordenadas
+        const x = Math.floor(Math.random()*100);
+        const y = Math.floor(Math.random()*100);
+        layer.push(`${x}vw ${y}vh 0 ${color}, ${x}vw ${y + 100}vh 0 ${color}`);
+    }
+
+    const container = document.querySelector(selector);
+    //agregar a la variable layer los strings de cordenadas
+    container.style.setProperty("--space-layer",layer.join(","));
+    container.style.setProperty("--size",size);
+    container.style.setProperty("--duration",duration);
+};
+
+generateSpaceLAyer("1px",".space-1", 300, "60s");
+generateSpaceLAyer("2px",".space-2", 400, "65s");
+generateSpaceLAyer("4px",".space-3", 400, "75s");
+
+//animacion para mover las estrellas
+
+const audioCarta = new Audio('./audio/cartaCorto.mp3');
+
+audioCarta.loop = true;
