36f73e157b2b2be6ca64573ec0da3baef056e8d0
Josue Caal <josuecaal787@gmail.com>
2024-05-08 17:57:40 -0600

    input checkbox

--- js/audios.js
+++ js/audios.js
@@ -1,2 +1,13 @@
-const audio = new Audio("audio/cartaCorto.mp3");
-audio.play();
+const audio = new Audio("./audio/cartaCorto.mp3");
+// audio.play();
+
+let musicInput=document.querySelector('#alternar-musica');
+
+audio.loop = true;
+musicInput = addEventListener('change',()=>{
+    if (audio.paused) {
+            audio.play();
+    } else {
+        audio.pause();
+    }
+});
